88d16520c5dfe9054e613b048c6ebd9b533770a8
Jolane Synott <jolane@jolane.net>
2018-03-23 19:52:05 +1100

    Adjustment to StyleSheetManager

--- src/cms/FunctionsPreview.js
+++ src/cms/FunctionsPreview.js
@@ -6,11 +6,11 @@ import Functions from '../sections/Functions'
 class FunctionsPreview extends React.Component {
   componentDidMount() {
     const iframe = document.querySelector('.nc-previewPane-frame')
-    const iframeHeadElem = iframe.contentDocument.head
+    this.iframeHeadElem = iframe.contentDocument.head
   }
   render() {
     return (
-      <StyleSheetManager target={iframeHeadElem}>
+      <StyleSheetManager target={this.iframeHeadElem}>
         <Functions
           title={props.entry.getIn(['data', 'title'])}
           text={props.entry.getIn(['data', 'text'])}
