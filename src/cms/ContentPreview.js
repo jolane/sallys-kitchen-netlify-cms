1e3d5badd0d2515499238f17a7a1b0f75c3f2fac
Jolane Synott <jolane@jolane.net>
2018-04-09 21:43:58 +1000

    Updated content preview

--- src/cms/ContentPreview.js
+++ src/cms/ContentPreview.js
@@ -12,42 +12,49 @@ const AboutPreview = ({ entry, widgetFor }) => {
   const iframeHeadElem = iframe.contentDocument.head
 
   return (
-    <StyleSheetManager target={iframeHeadElem}>
-      <About
-        about_title={entry.getIn(['data', 'about_title'])}
-        about_paragraph={entry.getIn(['data', 'about_paragraph'])}
-        about_large_text={entry.getIn(['data', 'about_large_text'])}
-        about_paragraph2={entry.getIn(['data', 'about_paragraph2'])}
-      />
-
-      <Images
-        image_one={entry.getIn(['data', 'image_one'])}
-        image_two={entry.getIn(['data', 'image_two'])}
-        image_three={entry.getIn(['data', 'image_three'])}
-        image_four={entry.getIn(['data', 'image_four'])}
-      />
-
-      <TheKitchen
-        kitchen_title={entry.getIn(['data', 'kitchen_title'])}
-        kitchen_text={entry.getIn(['data', 'kitchen_text'])}
-      />
-
-      <WhatsCooking
-        cooking_title={entry.getIn(['data', 'cooking_title'])}
-        cooking_text={entry.getIn(['data', 'cooking_text'])}
-      />
-
-      <WhatsCooking
-        cooking_title={entry.getIn(['data', 'cooking_title'])}
-        cooking_text={entry.getIn(['data', 'cooking_text'])}
-      />
-
-      <Where
-        where_title={entry.getIn(['data', 'where_title'])}
-        where_large_text={entry.getIn(['data', 'where_large_text'])}
-        where_paragraph={entry.getIn(['data', 'where_paragraph'])}
-      />
-    </StyleSheetManager>
+    <div>
+      <StyleSheetManager target={iframeHeadElem}>
+        <About
+          about_title={entry.getIn(['data', 'about_title'])}
+          about_paragraph={entry.getIn(['data', 'about_paragraph'])}
+          about_large_text={entry.getIn(['data', 'about_large_text'])}
+          about_paragraph2={entry.getIn(['data', 'about_paragraph2'])}
+        />
+      </StyleSheetManager>
+      <StyleSheetManager target={iframeHeadElem}>
+        <Images
+          image_one={entry.getIn(['data', 'image_one'])}
+          image_two={entry.getIn(['data', 'image_two'])}
+          image_three={entry.getIn(['data', 'image_three'])}
+          image_four={entry.getIn(['data', 'image_four'])}
+        />
+      </StyleSheetManager>
+      <StyleSheetManager target={iframeHeadElem}>
+        <TheKitchen
+          kitchen_title={entry.getIn(['data', 'kitchen_title'])}
+          kitchen_text={entry.getIn(['data', 'kitchen_text'])}
+        />
+      </StyleSheetManager>
+       <StyleSheetManager target={iframeHeadElem}>
+        <WhatsCooking
+          cooking_title={entry.getIn(['data', 'cooking_title'])}
+          cooking_text={entry.getIn(['data', 'cooking_text'])}
+        />
+      </StyleSheetManager>
+      <StyleSheetManager target={iframeHeadElem}>
+        <WhatsCooking
+          cooking_title={entry.getIn(['data', 'cooking_title'])}
+          cooking_text={entry.getIn(['data', 'cooking_text'])}
+        />
+      </StyleSheetManager>
+      <StyleSheetManager target={iframeHeadElem}>
+        <Where
+          where_title={entry.getIn(['data', 'where_title'])}
+          where_large_text={entry.getIn(['data', 'where_large_text'])}
+          where_paragraph={entry.getIn(['data', 'where_paragraph'])}
+        />
+      </StyleSheetManager>
+    </div>
   )
 }
 
