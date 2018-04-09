739edbb632a05dfd2eb65b8805bd099e04e5b731
Jolane Synott <jolane@jolane.net>
2018-04-09 22:09:30 +1000

    Added div

--- src/cms/ContentPreview.js
+++ src/cms/ContentPreview.js
@@ -14,44 +14,46 @@ const ContentPreview = ({ entry, widgetFor }) => {
 
   return (
     <StyleSheetManager target={iframeHeadElem}>
-      <About
-        about_title={entry.getIn(['data', 'about_title'])}
-        about_paragraph={entry.getIn(['data', 'about_paragraph'])}
-        about_large_text={entry.getIn(['data', 'about_large_text'])}
-        about_paragraph2={entry.getIn(['data', 'about_paragraph2'])}
-      />
+      <div>
+        <About
+          about_title={entry.getIn(['data', 'about_title'])}
+          about_paragraph={entry.getIn(['data', 'about_paragraph'])}
+          about_large_text={entry.getIn(['data', 'about_large_text'])}
+          about_paragraph2={entry.getIn(['data', 'about_paragraph2'])}
+        />
 
-      <Images
-        image_one={entry.getIn(['data', 'image_one'])}
-        image_two={entry.getIn(['data', 'image_two'])}
-        image_three={entry.getIn(['data', 'image_three'])}
-        image_four={entry.getIn(['data', 'image_four'])}
-      />
+        <Images
+          image_one={entry.getIn(['data', 'image_one'])}
+          image_two={entry.getIn(['data', 'image_two'])}
+          image_three={entry.getIn(['data', 'image_three'])}
+          image_four={entry.getIn(['data', 'image_four'])}
+        />
 
-      <TheKitchen
-        kitchen_title={entry.getIn(['data', 'kitchen_title'])}
-        kitchen_text={entry.getIn(['data', 'kitchen_text'])}
-      />
+        <TheKitchen
+          kitchen_title={entry.getIn(['data', 'kitchen_title'])}
+          kitchen_text={entry.getIn(['data', 'kitchen_text'])}
+        />
 
-      <WhatsCooking
-        cooking_title={entry.getIn(['data', 'cooking_title'])}
-        cooking_text={entry.getIn(['data', 'cooking_text'])}
-      />
+        <WhatsCooking
+          cooking_title={entry.getIn(['data', 'cooking_title'])}
+          cooking_text={entry.getIn(['data', 'cooking_text'])}
+        />
 
-      <WhatsCooking
-        cooking_title={entry.getIn(['data', 'cooking_title'])}
-        cooking_text={entry.getIn(['data', 'cooking_text'])}
-      />
+        <WhatsCooking
+          cooking_title={entry.getIn(['data', 'cooking_title'])}
+          cooking_text={entry.getIn(['data', 'cooking_text'])}
+        />
 
-      <Where
-        where_title={entry.getIn(['data', 'where_title'])}
-        where_large_text={entry.getIn(['data', 'where_large_text'])}
-        where_text={entry.getIn(['data', 'where_text'])}
-      />
-      <Hours
-        hours_title={entry.getIn(['data', 'hours_title'])}
-        hours_text={entry.getIn(['data', 'hours_text'])}
-      />
+        <Where
+          where_title={entry.getIn(['data', 'where_title'])}
+          where_large_text={entry.getIn(['data', 'where_large_text'])}
+          where_text={entry.getIn(['data', 'where_text'])}
+        />
+        <Hours
+          hours_title={entry.getIn(['data', 'hours_title'])}
+          hours_text={entry.getIn(['data', 'hours_text'])}
+        />
+      </div>
     </StyleSheetManager>
   )
 }
